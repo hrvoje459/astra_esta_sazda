2fc910a86107fc00523e33f9a71daac47b95079c
Hrvoje Rom <62212065+hrvoje459@users.noreply.github.com>
2020-09-25 16:24:01 +0200

    Update keySchedule.cpp

--- keySchedule.cpp
+++ keySchedule.cpp
@@ -28,27 +28,27 @@ void keySchedule(unsigned char** kljuc, unsigned char** prosireniKljuc) {
     cout << "kljuc: " << endl;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            kljuc[0][0] = 0x2b;
-            kljuc[0][1] = 0x7e;
-            kljuc[0][2] = 0x15;
-            kljuc[0][3] = 0x16;
+            /*  kljuc[0][0] = 0x49;
+             kljuc[0][1] = 0x20;
+             kljuc[0][2] = 0xe2;
+             kljuc[0][3] = 0x99;
 
-            kljuc[1][0] = 0x28;
-            kljuc[1][1] = 0xae;
-            kljuc[1][2] = 0xd2;
-            kljuc[1][3] = 0xa6;
+             kljuc[1][0] = 0xa5;
+             kljuc[1][1] = 0x20;
+             kljuc[1][2] = 0x52;
+             kljuc[1][3] = 0x61;
 
-            kljuc[2][0] = 0xab;
-            kljuc[2][1] = 0xf7;
-            kljuc[2][2] = 0x15;
-            kljuc[2][3] = 0x88;
+             kljuc[2][0] = 0x64;
+             kljuc[2][1] = 0x69;
+             kljuc[2][2] = 0x6f;
+             kljuc[2][3] = 0x47;
 
-            kljuc[3][0] = 0x09;
-            kljuc[3][1] = 0xcf;
-            kljuc[3][2] = 0x4f;
-            kljuc[3][3] = 0x3c;
+             kljuc[3][0] = 0x61;
+             kljuc[3][1] = 0x74;
+             kljuc[3][2] = 0x75;
+             kljuc[3][3] = 0x6e; */
 
-            cout << hex << setw(2) << setfill('0') << int(kljuc[i][j]) << " ";
+            cout << hex << setw(2) << setfill('0') << (kljuc[j][i]) << " ";
         }
     }
     cout << endl;
@@ -68,8 +68,9 @@ void keySchedule(unsigned char** kljuc, unsigned char** prosireniKljuc) {
             for (int j = 0; j < 4; j++) {
                 temp[j] = prosireniKljuc[i - 1][j];
             }
+
+            posmakni(temp);
             for (int j = 0; j < 4; j++) {
-                posmakni(temp);
                 prosireniKljuc[i][j] =
                     prosireniKljuc[i - n][j] ^ izSboxa(temp[j]) ^ (j == 0 ? rci[i / n] : 0);
             }
@@ -86,10 +87,14 @@ void keySchedule(unsigned char** kljuc, unsigned char** prosireniKljuc) {
 
     cout << "Prosireni kljuc: " << endl;
     for (int i = 0; i < 44; i++) {
+        if (i % 4 == 0) {
+            cout << endl;
+        }
+        // cout << dec << "w[" << setw(2) << setfill('0') << i << "] ";
         for (int j = 0; j < 4; j++) {
             cout << hex << setw(2) << setfill('0') << int(prosireniKljuc[i][j]) << " ";
         }
-        cout << endl;
+        // cout << endl;
     }
     cout << endl << endl;
 }
