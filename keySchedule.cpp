13e77e450f47c462d48af9dd7a875527ed677cac
Hrvoje Rom <62212065+hrvoje459@users.noreply.github.com>
2020-10-07 23:59:17 +0200

    Update keySchedule.cpp

--- keySchedule.cpp
+++ keySchedule.cpp
@@ -15,12 +15,7 @@ void popuniRconi(void) {
     for (int i = 2; i <= 10; i++) {
         rci[i] = podijeliPolinomski(pomnoziPolinomski(2, rci[i - 1]), 0x11b);
         rconi[i] = 0 ^ rci[i] << 24;
-        // cout << bitset<32>(rci[i]) << "  " << hex << rci[i] << endl;
-        // cout << bitset<32>(rconi[i]) << endl;
     }
-    /* for (int i = 1; i <= 10; i++) {
-        cout << hex << (int)(rci[i]) << "  ";
-    } */
     cout << endl;
 }
 
@@ -28,27 +23,7 @@ void keySchedule(unsigned char** kljuc, unsigned char** prosireniKljuc) {
     cout << "kljuc: " << endl;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            /*  kljuc[0][0] = 0x49;
-             kljuc[0][1] = 0x20;
-             kljuc[0][2] = 0xe2;
-             kljuc[0][3] = 0x99;
-
-             kljuc[1][0] = 0xa5;
-             kljuc[1][1] = 0x20;
-             kljuc[1][2] = 0x52;
-             kljuc[1][3] = 0x61;
-
-             kljuc[2][0] = 0x64;
-             kljuc[2][1] = 0x69;
-             kljuc[2][2] = 0x6f;
-             kljuc[2][3] = 0x47;
-
-             kljuc[3][0] = 0x61;
-             kljuc[3][1] = 0x74;
-             kljuc[3][2] = 0x75;
-             kljuc[3][3] = 0x6e; */
-
-            cout << hex << setw(2) << setfill('0') << (kljuc[j][i]) << " ";
+            cout << hex << setw(2) << setfill('0') << (int)kljuc[j][i] << " ";
         }
     }
     cout << endl;
@@ -61,22 +36,19 @@ void keySchedule(unsigned char** kljuc, unsigned char** prosireniKljuc) {
     for (int i = 0; i < 44; i++) {
         if (i < n) {
             for (int j = 0; j < 4; j++) {
-                prosireniKljuc[i][j] = kljuc[i][j];
+                prosireniKljuc[i][j] = kljuc[j][i];
             }
         } else if (i >= n && i % n == 0) {
             unsigned char temp[4];
             for (int j = 0; j < 4; j++) {
                 temp[j] = prosireniKljuc[i - 1][j];
             }
+            cout << endl;
 
             posmakni(temp);
             for (int j = 0; j < 4; j++) {
                 prosireniKljuc[i][j] =
                     prosireniKljuc[i - n][j] ^ izSboxa(temp[j]) ^ (j == 0 ? rci[i / n] : 0);
-            }
-        } else if (i >= n && n > 6 && i % n == 4) {
-            for (int j = 0; j < 4; j++) {
-                prosireniKljuc[i][j] = prosireniKljuc[i - n][j] ^ izSboxa(prosireniKljuc[i - 1][j]);
             }
         } else {
             for (int j = 0; j < 4; j++) {
@@ -84,20 +56,19 @@ void keySchedule(unsigned char** kljuc, unsigned char** prosireniKljuc) {
             }
         }
     }
-
-    cout << "Prosireni kljuc: " << endl;
-    for (int i = 0; i < 44; i++) {
-        if (i % 4 == 0) {
-            cout << endl;
-        }
-        // cout << dec << "w[" << setw(2) << setfill('0') << i << "] ";
-        for (int j = 0; j < 4; j++) {
-            cout << hex << setw(2) << setfill('0') << int(prosireniKljuc[i][j]) << " ";
-        }
-        // cout << endl;
-    }
-    cout << endl << endl;
 }
 
-void RoundKeyAdd(unsigned char** cypherText) {}
-void inverseRoundKeyAdd(unsigned char** cypherText) {}
+void RoundKeyAdd(unsigned char** cypherText, unsigned char** prosireniKljuc, int runda) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cypherText[i][j] = cypherText[i][j] ^ prosireniKljuc[runda * 4 + j][i];
+        }
+    }
+}
+void inverseRoundKeyAdd(unsigned char** cypherText, unsigned char** prosireniKljuc, int runda) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cypherText[i][j] = cypherText[i][j] ^ prosireniKljuc[runda * 4 + j][i];
+        }
+    }
+}
