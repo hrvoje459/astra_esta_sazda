8cbdc1a4f1614a93e6dde24056c6aa1d3fa46374
Hrvoje Rom <62212065+hrvoje459@users.noreply.github.com>
2020-10-08 00:00:15 +0200

    Update aesHeader.h

--- aesHeader.h
+++ aesHeader.h
@@ -3,14 +3,11 @@
 void byteSub(unsigned char** cypherText);
 void inverseByteSub(unsigned char** cypherText);
 
-
 unsigned char izSboxa(unsigned char ulaz);
 unsigned char izSboxaInverz(unsigned char ulaz);
 
 // POSMAKNI REDOVE
 void posmakni(unsigned char* cypherText);
-
-
 void rowShift(unsigned char** cypherText);
 void inverseRowShift(unsigned char** unCypherText);
 
@@ -18,10 +15,9 @@ void inverseRowShift(unsigned char** unCypherText);
 void columnMix(unsigned char** cypherText);
 void inverseColumnMix(unsigned char** unCypherText);
 
-
 // DODAJ KLJUCEVE
-void RoundKeyAdd(unsigned char** cypherText);
-void inverseRoundKeyAdd(unsigned char** cypherText);
+void RoundKeyAdd(unsigned char** cypherText, unsigned char** prosireniKljuc, int i);
+void inverseRoundKeyAdd(unsigned char** cypherText, unsigned char** prosireniKljuc, int i);
 
 // KEY SCHEDULE
 void keySchedule(unsigned char** kljuc, unsigned char** prosireniKljuc);
@@ -30,4 +26,3 @@ void keySchedule(unsigned char** kljuc, unsigned char** prosireniKljuc);
 unsigned short int gPoljePomnozi(unsigned short a, unsigned short b);
 unsigned short int podijeliPolinomski(unsigned short int djeljenik, unsigned short int djelitelj);
 unsigned short int pomnoziPolinomski(unsigned short int a, unsigned short int b);
-
